51ec085b25b5fd003c087b1e3ff8997e81632901
lukmanhidayah <lukmanhidayah01@gmail.com>
2020-08-21 22:44:18 +0700

    adding delete function to item

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 
 //import components needed
 import GoalItem from './components/GoalItem.component';
@@ -9,11 +9,20 @@ import GoalInput from './components/GoalInput.component';
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
 
+  //all functions on App's component
   const addGoalHandler = goalTitles => {
     // arrow function in function before for guarantee the latest snapshoot before it then applies your state
     setCourseGoals(currentGoals => [...courseGoals, { id: Math.random().toString(), value: goalTitles }])
   }
 
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      // if list on click, items will show all of item who is not same on clicked id
+      return currentGoals.filter((goal) => goal.id !== goalId)
+    })
+  };
+
+  // render ui
   return (
     <View style={styles.screen}>
       <GoalInput onAddGoal={addGoalHandler} />
@@ -21,7 +30,7 @@ export default function App() {
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
         renderItem={
-          itemData => <GoalItem title={itemData.item.value} />
+          itemData => (<GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />)
         }
       >
       </FlatList>
